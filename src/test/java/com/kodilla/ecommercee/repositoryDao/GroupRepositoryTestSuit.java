a99e833bd929da512d5643128fc4318918f8ba09
godyn <godyn.ola@gmail.com>
2019-06-10 23:03:23 +0200

    JDP190501-13

--- src/test/java/com/kodilla/ecommercee/repositoryDao/GroupRepositoryTestSuit.java
+++ src/test/java/com/kodilla/ecommercee/repositoryDao/GroupRepositoryTestSuit.java
@@ -3,13 +3,16 @@ package com.kodilla.ecommercee.repositoryDao;
 import com.kodilla.ecommercee.domain.Group;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
 import java.util.Optional;
 
-@SpringBootTest()
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class GroupRepositoryTestSuit {
 
     @Autowired
