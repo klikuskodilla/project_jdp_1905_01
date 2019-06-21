438454754750b8a3c1debc2bc2c8ce1b53e63337
godyn <godyn.ola@gmail.com>
2019-06-21 14:17:22 +0200

    JDP190501-1

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,41 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.domain.Dto.ProductDto;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProducts")
+    public List<ProductDto> getProducts(){
+        return new ArrayList<>();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "getProduct")
+    public ProductDto getProduct(Long productId) throws ProductNotFoundException{
+        return new ProductDto(1L, "test_product_name", "test_details", 24.5);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createProduct", consumes = APPLICATION_JSON_VALUE)
+    public void createProduct(ProductDto productDto){
+
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
+    public ProductDto updateProduct(ProductDto productDto){
+        return new ProductDto(1L, "updated_test_product_name", "updated_test_details" , 53.5);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
+    public void deleteProduct(Long productId){
+
+    }
 }
